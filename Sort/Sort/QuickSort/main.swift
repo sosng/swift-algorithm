0c807e10325ba14208fe146f406e65bdca3c7ffe
sayaka <sosngg@gmail.com>
2018-05-22 23:50:15 +0800

    add quick sourt

---
+++ Sort/Sort/QuickSort/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  QuickSort
+//
+//  Created by Sayaka on 2018/5/22.
+//  Copyright © 2018 songshuang. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
