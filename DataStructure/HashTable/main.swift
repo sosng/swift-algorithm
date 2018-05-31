23803eb648f7c54a76b86cc1cff774046072fb75
sayaka <sosngg@gmail.com>
2018-05-31 17:25:42 +0800

    update

--- DataStructure/HashTable/main.swift
+++ DataStructure/HashTable/main.swift
@@ -10,3 +10,18 @@ import Foundation
 
 //https://github.com/raywenderlich/swift-algorithm-club/tree/master/Hash%20Table
 
+
+public class ListNode {
+      public var val: Int
+      public var next: ListNode?
+      public init(_ val: Int) {
+        self.val = val
+        self.next = nil
+    }
+}
+ 
+class Solution {
+    func addTwoNumbers(_ l1: ListNode?, _ l2: ListNode?) -> ListNode? {
+        
+    }
+}
