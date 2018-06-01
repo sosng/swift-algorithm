225b42e3a889dde36dc3e077e7a89290e93c6b2c
sayaka <sosngg@gmail.com>
2018-06-01 16:45:41 +0800

    update hashtable

--- DataStructure/HashTable/main.swift
+++ DataStructure/HashTable/main.swift
@@ -10,23 +10,73 @@ import Foundation
 
 //https://github.com/raywenderlich/swift-algorithm-club/tree/master/Hash%20Table
 
-
-public class ListNode {
-      public var val: Int
-      public var next: ListNode?
-      public init(_ val: Int) {
-        self.val = val
-        self.next = nil
+public struct HashTable<Key: Hashable, Value> {
+    private typealias Element = (key: Key, value: Value)
+    private typealias Bucket = [Element]
+    private var buckets: [Bucket]
+    
+    private(set) public var count = 0
+    
+    public func index(forKey key: Key) -> Int {
+        return abs(key.hashValue) % buckets.count
     }
-}
- 
-class Solution {
-    func addTwoNumbers(_ l1: ListNode?, _ l2: ListNode?) -> ListNode? {
-        var r = ListNode(0)
-        let sum = (l1?.val ?? 0) + (l2?.val ?? 0)
-        let carry = sum >= 10 ? 1 : 0
-        let value = sum >= 10 ? (sum - 10) : sum
+    
+    public func value(forKey key: Key) -> Value? {
+        let index = self.index(forKey: key)
+        for element in buckets[index] {
+            if element.key == key {
+                return element.value
+            }
+        }
+        return nil
+    }
+    
+    @discardableResult
+    public mutating func updateValue(_ value: Value, forKey key: Key) -> Value? {
+        let index = self.index(forKey: key)
+        for (i, element) in buckets[index].enumerated() {
+            if element.key == key {
+                let oldValue = element.value
+                buckets[index][i].value = value
+                return oldValue
+            }
+        }
         
-        return r
+        buckets[index].append((key: key, value: value))
+        count += 1
+        return nil
     }
+    
+    @discardableResult
+    public mutating func removeValue(forKey key: Key) -> Value? {
+        let index = self.index(forKey: key)
+        for (i, element) in buckets[index].enumerated() {
+            if element.key == key {
+                buckets[index].remove(at: i)
+                count -= 1
+                return element.value
+            }
+        }
+        return nil
+    }
+    
+    public subscript(key: Key) -> Value? {
+        get {
+            return value(forKey: key)
+        }
+        set {
+            if let value = newValue {
+                updateValue(value, forKey: key)
+            } else {
+                removeValue(forKey: key)
+            }
+        }
+    }
+    
+    init(capacity: Int) {
+        assert(capacity > 0)
+        buckets = Array<Bucket>(repeating: [], count: capacity)
+    }
+    
 }
+
